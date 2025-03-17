ad95d6495ff8b02f9095c5eab9a3652abdb0460d
Muhammad Muneeb <nadeemmk1900@gmail.com>
2025-03-17 16:32:04 +0500

    all graphql

---
+++ newGraph/fakeDb.js
@@ -0,0 +1,41 @@
+export const users = [
+  {
+    id: 1234,
+    name: "John Doe",
+    email: "john.doe@example.com",
+    age: 25,
+  },
+  {
+    id: 22345,
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    age: 30,
+  },
+  {
+    id: 3345,
+    name: "Alice Johnson",
+    email: "alice.johnson@example.com",
+    age: 28,
+  },
+];
+
+export const quotes = [
+  {
+    id: 1234,
+    quote:
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    author: "Albert Einstein",
+  },
+  {
+    id: 22345,
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Confucius",
+  },
+  {
+    id: 3345,
+    quote:
+      "Suffering has been stronger than all other teaching, and has taught me that I am the only one who can change the world.",
+    author: "Mahatma Gandhi",
+  },
+];
